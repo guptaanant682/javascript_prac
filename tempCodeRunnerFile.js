827503be691d96b625ab4bac320ca993a1ee2fb4
Infinity <85234537+guptaanant682@users.noreply.github.com>
2023-12-24 21:12:50 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,15 @@
+function findSum(n){
+    let ans = 0;
+    for (let i=0; i<n; i++){
+        ans += i;
+
+    }
+    return ans;
+}
+
+function findSumTill100(){
+    console.log(findSum(100));
+}
+
+setTimeout(findSumTill100, 1000)//cpu will proceed, it will not wait here for 1 second
+console.log("Hello World!");
