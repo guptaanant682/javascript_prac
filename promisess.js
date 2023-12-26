9706986128084fb6f318fd2886c7f0d3a6fa1eeb
Infinity <85234537+guptaanant682@users.noreply.github.com>
2023-12-26 23:22:58 +0530

    Update promisess.js

--- promisess.js
+++ promisess.js
@@ -159,3 +159,22 @@ async function consumePromiseFive(){
 // }
 
 consumePromiseFive();
+
+
+
+////////////////////////////////////////////////////////////////////////////
+
+// function myOwnSetTimeout(duration){
+//     let p = new Promise(function(resolve){
+//         setTimeout(resolve, 1000)
+//     });
+//     return p;
+// }
+
+// myOwnSetTimeout(1000)
+// .then(function(){
+//     console.log("log the first thing");
+// })
+
+
+
