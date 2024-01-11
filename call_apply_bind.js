c99498ac22add5efb1154e6cff368e72e9100be3
Infinity <85234537+guptaanant682@users.noreply.github.com>
2024-01-11 19:02:09 +0530

    Update call_apply_bind.js

--- call_apply_bind.js
+++ call_apply_bind.js
@@ -1,23 +1,24 @@
 //  call apply bind
 //  ye teen tareke hai function ko call karne ke kisi object ko this maan kar
 
-function abcd(){
-    console.log(this);  // window
-}
-abcd();
-// the value of this function is window here
+// function abcd(){
+//     console.log(this);  // window
+// }
+// abcd();
+// // the value of this function is window here
 
 
 
 
 
-const obj = {name: "harsh"}
+// const obj = {name: "harsh"}
 
 function abcd(){
     console.log(this);  // window
 }
 abcd.call(obj);//if you want that the value of function should be object instead of than do this method
 //when calling the function write ".call"  and pass whatever you want to be the value of this function
+
 
 
 const obj2 = {name: "harsh"}
@@ -40,6 +41,19 @@ function abcd(a,b,c){
 abcd.apply(obj3, [1,2,3]); //apply mein paheli value this deni h fir comma lgake dusri values array k form mein deni h jiske andr hum parameters ki value pass kr skte h
 
 ///////////////////////////////////////////bind/////////////
+
+
+const obj2 = {name: "harsh"}
+
+function abcd(){
+    console.log(this);  // window
+}
+
+Bind funciton ko chlata nhi hai apko ek aur function deta h return krke jisko aap badme chla skte ho
+
+const baadmechalanekeliyefunc = abcd.bind(obj);
+baadmechalanekeliyefunc();
+// ab iska answer aaega
 
 
 
